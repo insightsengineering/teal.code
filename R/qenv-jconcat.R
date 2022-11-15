e7900a12deaceaf308f9a61f0b0c2d1d06796a3c
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-11-15 11:12:09 +0100

    concatenate method - qenv

---
+++ R/qenv-jconcat.R
@@ -0,0 +1,49 @@
+#' Concatenate two `qenv` objects
+#'
+#' Combine two `qenv` objects by simple concatenate their environments and the code.
+#' We recommend to use the `join` method for most cases.
+#' @param x (`qenv`)
+#' @param y (`qenv`)
+#' @examples
+#' q1 <- new_qenv(
+#'   code = c(iris1 = "iris1 <- iris", mtcars1 = "mtcars1 <- mtcars"),
+#'   env = list2env(list(
+#'     iris1 = iris,
+#'     mtcars1 = mtcars
+#'   ))
+#' )
+#' q2 <- q1
+#' q1 <- eval_code(q1, "iris2 <- iris")
+#' q2 <- eval_code(q2, "mtcars2 <- mtcars")
+#' qq <- concat(q1, q2)
+#' get_code(qq)
+#' @export
+setGeneric("concat", function(x, y) {
+  standardGeneric("concat")
+})
+
+#' @rdname concat
+#' @export
+setMethod("concat", signature = c("qenv", "qenv"), function(x, y) {
+  y@id <- c(x@id, y@id)
+  y@code <- c(x@code, y@code)
+  y@warnings <- c(x@warnings, y@warnings)
+  y@messages <- c(x@messages, y@messages)
+
+  # insert (and overwrite) objects from y to x
+  y@env <- rlang::env_clone(y@env, parent = parent.env(.GlobalEnv))
+  rlang::env_coalesce(env = y@env, from = x@env)
+  y
+})
+
+#' @rdname concat
+#' @export
+setMethod("concat", signature = c("qenv.error"), function(x, y) {
+  x
+})
+
+#' @rdname concat
+#' @export
+setMethod("concat", signature = c("qenv", "qenv.error"), function(x, y) {
+  y
+})
