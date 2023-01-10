429c806d83a4694636e48996d96c8836715f8292
Nikolas Burkoff <nikolas.burkoff@roche.com>
2023-01-10 08:38:14 +0000

    Use rlang::env_print (#101)

--- R/qenv-show.R
+++ R/qenv-show.R
@@ -5,14 +5,13 @@
 #' @return nothing
 #' @importFrom methods show
 #' @examples
-#' q1 <- new_qenv(code = "print('a')", env = new.env())
+#' q1 <- new_qenv(
+#'   code = "a <- 5
+#'     b <- data.frame(x = 1:10)",
+#'   env = list2env(list(a = 5, b = data.frame(x = 1:10)))
+#' )
 #' q1
 #' @export
 setMethod("show", "qenv", function(object) {
-  obs <- names(as.list(object@env))
-  if (length(obs) > 0) {
-    cat(sprintf("A qenv object containing: %s\n", paste(obs, collapse = ", ")))
-  } else {
-    cat("A qenv object containing no objects.\n")
-  }
+  rlang::env_print(object@env)
 })
