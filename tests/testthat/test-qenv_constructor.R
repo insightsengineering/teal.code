5d01a3bf8bf07474f49c2bee3392df8b396dd9dc
Marcin <133694481+m7pr@users.noreply.github.com>
2023-10-04 10:25:43 +0200

    Update tests/testthat/test-qenv_constructor.R
    Co-authored-by: Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat/test-qenv_constructor.R
+++ tests/testthat/test-qenv_constructor.R
@@ -2,7 +2,7 @@ testthat::test_that("constructor returns qenv if nothing is specified", {
   q <- new_qenv()
   testthat::expect_s4_class(q, "qenv")
   testthat::expect_identical(ls(q@env), character(0))
-  testthat::expect_identical(q@code, character())
+  testthat::expect_identical(q@code, character(0))
   testthat::expect_identical(q@id, integer(0))
   testthat::expect_identical(q@warnings, character(0))
   testthat::expect_identical(q@messages, character(0))
