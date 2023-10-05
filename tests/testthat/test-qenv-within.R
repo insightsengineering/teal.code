668bcda1186b731c7396759d783092b89799cf47
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-10-05 10:42:30 +0200

    improve tests for value injection

--- tests/testthat/test-qenv-within.R
+++ tests/testthat/test-qenv-within.R
@@ -79,44 +79,41 @@ testthat::test_that("differently formulated expressions yield the same code", {
 testthat::test_that("external values can be injected into expressions through `...`", {
   q <- new_qenv()
 
-  q <- within(q, {
-    i <- subset(iris, Species == "setosa")
+  q1 <- within(q, {
+    i <- subset(iris, Species == "virginica")
   })
 
-  q <- within(q, {
-    ii <- subset(iris, Species == species)
+  q2 <- within(q, {
+    i <- subset(iris, Species == species)
   },
   species = "virginica")
 
-  external_value <- "versicolor"
-  q <- within(q, {
-    iii <- subset(iris, Species == species)
+  external_value <- "virginica"
+  q3 <- within(q, {
+    i <- subset(iris, Species == species)
   },
   species = external_value)
 
-  testthat::expect_identical(
-    get_code(q),
-    c(
-      "i <- subset(iris, Species == \"setosa\")",
-      "ii <- subset(iris, Species == \"virginica\")",
-      "iii <- subset(iris, Species == \"versicolor\")"
-    )
-  )
+  testthat::expect_identical(get_code(q1), get_code(q2))
+  testthat::expect_identical(get_code(q1), get_code(q3))
+})
 
+testthat::test_that("external values are not taken from calling frame", {
   q <- new_qenv()
   species <- "setosa"
   qq <- within(q, {
     i <- subset(iris, Species == species)
   })
   testthat::expect_s3_class(qq, "qenv.error")
+  testthat::expect_error(get_code(qq), "object 'species' not found")
 
   qq <- within(q, {
     i <- subset(iris, Species == species)
   },
   species = species)
   testthat::expect_s4_class(qq, "qenv")
+  testthat::expect_identical(get_code(qq), "i <- subset(iris, Species == \"setosa\")")
 })
-
 
 # return value ----
 testthat::test_that("within.qenv renturns a deep copy of `data`", {
