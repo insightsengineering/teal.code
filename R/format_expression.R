263843d55fc1754ad84af934a9b21cf7a8423fee
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-10-05 13:47:49 +0200

    format_expression alternative

---
+++ R/format_expression.R
@@ -0,0 +1,52 @@
+
+# recursively convert language object ot list of simple calls
+lang2calls <- function(x) {
+  if (is.call(x)) {
+    if (identical(as.list(x)[[1L]], as.symbol("{"))) {
+      as.list(x)[-1L]
+    } else {
+      list(x)
+    }
+  }
+  else {
+    unlist(lapply(x, lang2calls))
+  }
+}
+
+# format expression into one string, generally one call per line
+format_expression <- function(code) {
+  code <- lang2calls(code)
+  paste(code, collapse = "\n")
+}
+
+# examples
+expr1 <- expression({
+  i <- iris
+  m <- mtcars
+})
+expr2 <- expression(
+  i <- iris,
+  m <- mtcars
+)
+cll1 <- substitute({
+  i <- iris
+  m <- mtcars
+})
+cll2 <- list(
+  quote(i <- iris),
+  quote(m <- mtcars)
+)
+
+# function definition
+fundef <- quote(
+  format_expression <- function(x) {
+    x + x
+    return(x)
+  }
+)
+
+format_expression(expr1)
+format_expression(expr2)
+format_expression(cll1)
+format_expression(cll2)
+format_expression(fundef)
