1765b1436c62859d7ba9470d6cf3be2ac9652189
go_gonzo <dawid.kaledkowski@gmail.com>
2023-10-06 10:35:01 +0200

    Necessary fixes

--- R/qenv-show.R
+++ R/qenv-show.R
@@ -4,6 +4,7 @@
 #' @param object (`qenv`)
 #' @return nothing
 #' @importFrom methods show
+#' @exportMethod show
 #' @examples
 #' q1 <- new_qenv(
 #'   code = "a <- 5
@@ -11,7 +12,6 @@
 #'   env = list2env(list(a = 5, b = data.frame(x = 1:10)))
 #' )
 #' q1
-#' @export
 setMethod("show", "qenv", function(object) {
   rlang::env_print(object@env)
 })
