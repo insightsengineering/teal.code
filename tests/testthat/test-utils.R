ab35a74b8b51dfed43181816960fa83a9da6c1c2
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-10-06 16:10:59 +0200

    improve tests for lang2calls

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,6 +10,58 @@ test_that("dev_suppress function supress printing plot on IDE", {
 
 
 # lang2calls ------------------------------------------------------------------------------------------------------
+testthat::test_that("lang2calls returns list of calls given a language object", {
+  expr1 <- expression({
+    i <- iris
+    m <- mtcars
+  })
+  expr2 <- expression(
+    i <- iris,
+    m <- mtcars
+  )
+  call1 <- quote(
+    i <- iris
+  )
+  call2 <- quote({
+    i <- iris
+    m <- mtcars
+  })
+
+  testthat::expect_true(is.list(lang2calls(expr1)) && all(vapply(lang2calls(expr1), is.call, logical(1L))))
+  testthat::expect_true(is.list(lang2calls(expr2)) && all(vapply(lang2calls(expr2), is.call, logical(1L))))
+  testthat::expect_true(is.list(lang2calls(call1)) && all(vapply(lang2calls(call1), is.call, logical(1L))))
+  testthat::expect_true(is.list(lang2calls(call2)) && all(vapply(lang2calls(call2), is.call, logical(1L))))
+})
+
+testthat::test_that("lang2calls returns list of calls given a list of language objects", {
+  exprlist <- list(
+    expression(i <- iris),
+    expression({
+      i <- iris
+      m <- mtcars
+    })
+  )
+  calllist <- list(
+    quote(i <- iris),
+    quote({
+      i <- iris
+      m <- mtcars
+    })
+  )
+
+  testthat::expect_true(is.list(lang2calls(exprlist)) && all(vapply(lang2calls(exprlist), is.call, logical(1L))))
+  testthat::expect_true(is.list(lang2calls(calllist)) && all(vapply(lang2calls(calllist), is.call, logical(1L))))
+})
+
+testthat::test_that("lang2calls returns atomics and symbols as is", {
+  testthat::expect_identical(lang2calls("x"), "x")
+  testthat::expect_identical(lang2calls(as.symbol("x")), as.symbol("x"))
+
+  testthat::skip(message = "unexplained behavior")
+  testthat::expect_identical(lang2calls(list("x")), "x")
+  testthat::expect_identical(lang2calls(list(as.symbol("x"))), as.symbol("x"))
+})
+
 
 testthat::test_that(
   "format_expression turns expression/calls or lists thereof into character strings without curly brackets",
