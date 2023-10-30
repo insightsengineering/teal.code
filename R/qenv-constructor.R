b0d22a93d9097ec38e0eb2d3209593fcc086a513
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-10-30 14:24:16 +0100

    rename qenv constructor

--- R/qenv-constructor.R
+++ R/qenv-constructor.R
@@ -6,15 +6,15 @@
 #' Any changes must be made by evaluating code in it with `eval_code` or `within`,
 #' thereby ensuring reproducibility.
 #'
-#' @name new_qenv
+#' @name qenv
 #'
 #' @examples
-#' new_qenv()
+#' qenv()
 #'
 #' @return `qenv` object.
 #'
 #' @export
-new_qenv <- function() {
+qenv <- function() {
   q_env <- new.env(parent = parent.env(.GlobalEnv))
   lockEnvironment(q_env, bindings = TRUE)
   methods::new("qenv", env = q_env)
