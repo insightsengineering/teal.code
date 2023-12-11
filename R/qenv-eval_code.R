b7669b36c08f0450ac6f591e2a367733e4c4b295
m7pr <marcin.kosinski.mk1@roche.com>
2023-12-11 13:56:39 +0100

    bring back full code to trace for qenv.error

--- R/qenv-eval_code.R
+++ R/qenv-eval_code.R
@@ -61,7 +61,7 @@ setMethod("eval_code", signature = c("qenv", "character"), function(object, code
               deparse1(single_call)
             ),
             class = c("qenv.error", "try-error", "simpleError"),
-            trace = single_call
+            trace = object@code
           )
         }
       ),
