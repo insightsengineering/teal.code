d16ddd7dae05103bb4b7dd52d5a2be557fc30d11
kartikeya kirar <kirar.kartikeya1@gmail.com>
2024-01-11 00:56:57 +0530

    Removing old rd syntax (#191)
    this is part of
    https://github.com/insightsengineering/teal.code/pull/189
    
    I noticed some old syntax that was not previously checked, so I am
    creating a separate pull request for it.

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,13 @@
 #' Suppresses plot display in the IDE by opening a PDF graphics device
 #'
-#' This function opens a PDF graphics device using \code{\link[grDevices]{pdf}} to suppress
+#' This function opens a PDF graphics device using [`grDevices::pdf`] to suppress
 #' the plot display in the IDE. The purpose of this function is to avoid opening graphic devices
 #' directly in the IDE.
 #'
 #' @param x lazy binding which generates the plot(s)
 #'
-#' @details The function uses \code{\link[base]{on.exit}} to ensure that the PDF graphics
-#'          device is closed (using \code{\link[grDevices]{dev.off}}) when the function exits,
+#' @details The function uses [`base::on.exit`] to ensure that the PDF graphics
+#'          device is closed (using [`grDevices::dev.off`]) when the function exits,
 #'          regardless of whether it exits normally or due to an error. This is necessary to
 #'          clean up the graphics device properly and avoid any potential issues.
 #'
