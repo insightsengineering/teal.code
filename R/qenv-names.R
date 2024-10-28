72f059511a97710398fef841cb793d5fa3fd7bd3
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-10-28 17:24:20 +0000

    fix: warning in R CMD check

--- R/qenv-names.R
+++ R/qenv-names.R
@@ -8,23 +8,39 @@
 #' unless `all.names` parameter is set to `TRUE`.
 #'
 #' @param x A (`qenv` or `qenv_error`) object.
-#' @param all.names (`logical(1)`) that specifies whether to include hidden
-#' objects.
+#' @param ... Additional parameters to this function, allowed parameters:
+#'
+#' - `all.names`: (`logical(1)`)that specifies whether to include hidden objects.
 #' @param value Does nothing as the names assignment is not supported.
 #'
 #' @return A character vector of names.
 #'
 #' @seealso [base::names()]
 #'
+#' @examples
+#' q1 <- within(qenv(), iris <- iris)
+#' names(q1)
+#'
+#' q2 <- within(q1, {
+#'   mtcars <- mtcars
+#'   CO2 <- CO2
+#' })
+#' names(q2)
+#'
 #' @export
-names.qenv <- function(x, all.names = FALSE) {
-  checkmate::assert_flag(all.names)
-  ls(get_env(x), all.names = all.names)
+names.qenv <- function(x, ...) {
+  dots <- rlang::list2(...)
+  if (length(setdiff(names(dots), "all.names")) > 0) {
+    stop("Only `x` and 'all.names' parameter are allowed")
+  }
+  checkmate::assert_flag(dots[["all.names"]], .var.name = "all.names", null.ok = TRUE)
+  if (is.null(dots[["all.names"]])) dots[["all.names"]] <- FALSE
+  ls(get_env(x), all.names = dots[["all.names"]])
 }
 
 #' @rdname names.qenv
 #' @export
-names.qenv.error <- function(x, all.names = FALSE) {
+names.qenv.error <- function(x) {
   NULL
 }
 
