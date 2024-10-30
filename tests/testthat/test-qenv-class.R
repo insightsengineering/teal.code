9904570ea31d4f96c14e9ef431c0538be0a1b314
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-10-30 15:38:40 +0000

    fix: complete tests for qenv-class

--- tests/testthat/test-qenv-class.R
+++ tests/testthat/test-qenv-class.R
@@ -1,13 +1,20 @@
 testthat::describe("methods::new(qenv)", {
   testthat::it("creates a locked environment", {
-    expect_true(is.environment(methods::new("qenv")))
+    expect_true(environmentIsLocked(teal.code::get_env(methods::new("qenv"))))
+  })
+
+  testthat::it("creates a locked environment when .xData is manually defined", {
+    new_env <- new.env()
+    expect_false(environmentIsLocked(new_env))
+
+    expect_true(environmentIsLocked(teal.code::get_env(methods::new("qenv"))))
   })
 
   testthat::it("throws error when id and code length doesn't match", {
-    expect_error(is.environment(methods::new("qenv", id = 1)))
+    expect_error(methods::new("qenv", id = 1L), "@code and @id slots must have the same length\\.")
   })
 
   testthat::it("throws error when .xData is not an environment", {
-    expect_true(is.environment(methods::new("qenv")))
+    expect_error(methods::new("qenv", .xData = 2), "Must be an environment, not 'double'\\.")
   })
 })
