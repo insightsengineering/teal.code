f18d55acfe39b00fd41cc72011c27813dae9ddd5
m7pr <marcin.kosinski.mk1@roche.com>
2024-11-04 15:03:36 +0100

    bring @param names documentation

--- R/qenv-constructor.R
+++ R/qenv-constructor.R
@@ -5,6 +5,10 @@
 #'
 #' Create a `qenv` object and evaluate code in it to track code history.
 #'
+#' @param names (`character`) for `x[names]`, names of objects included in `qenv` to subset. Names not present in `qenv`
+#' are skipped. For `get_code` `r lifecycle::badge("experimental")` vector of object names to return the code for.
+#' For more details see the "Extracting dataset-specific code" section.
+#'
 #' @details
 #'
 #' `qenv()` instantiates a `qenv` with an empty environment.
