ffa54c8cb7be3e0e285ce72286a4240663b55470
m7pr <marcin.kosinski.mk1@roche.com>
2024-11-08 09:57:18 +0100

    allow to pass ... trhough [. to get_code_dependency - needed in teal.data

--- R/qenv-extract.R
+++ R/qenv-extract.R
@@ -1,6 +1,7 @@
 #'
 #' @section Subsetting:
 #' `x[names]` subsets objects in `qenv` environment and limit the code to the necessary needed to build limited objects.
+#' `...` passes parameters to further methods.
 #'
 #' @param x (`qenv`)
 #'
@@ -15,7 +16,7 @@
 #' @rdname qenv
 #'
 #' @export
-`[.qenv` <- function(x, names) {
+`[.qenv` <- function(x, names, ...) {
   checkmate::assert_class(names, "character")
   possible_names <- ls(get_env(x), all.names = TRUE)
   names_warn <- setdiff(names, possible_names)
@@ -43,7 +44,7 @@
 
   x@env <- list2env(mget(x = names, envir = get_env(x)))
   names <- gsub("^`(.*)`$", "\\1", names)
-  x@code <- get_code_dependency(x@code, names = names)
+  x@code <- get_code_dependency(x@code, names = names, ...)
 
   x
 }
