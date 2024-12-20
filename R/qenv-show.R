6cbea730189f67194b40d52636b2740e9ea57938
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2024-12-20 10:43:23 +0100

    grey put hidden objects

--- R/qenv-show.R
+++ R/qenv-show.R
@@ -16,5 +16,26 @@
 #' @importFrom methods show
 #' @export
 setMethod("show", "qenv", function(object) {
-  rlang::env_print(object@.xData)
+  env <- get_env(object)
+  header <- cli::col_blue(sprintf("<environment: %s>", rlang::env_label(env)))
+  locked <- if (environmentIsLocked(env)) {
+    " [L]"
+  } else {
+    ""
+  }
+  parent <- sprintf("<environment: %s>", rlang::env_label(rlang::env_parent(env)))
+  cat(cli::style_bold(paste0(header, locked)), sep = "\n")
+  cat(parent, "\n")
+
+  shown <- ls(object)
+  lapply(shown, function(x) {
+    cat(sprintf("- %s: [%s]\n", x, class(object[[x]])[1]))
+  })
+
+  hidden <- setdiff(ls(object, all.names = TRUE), shown)
+  lapply(hidden, function(x) {
+    cat(cli::style_blurred(sprintf("- %s: [%s]\n", x, class(object[[x]])[1])))
+  })
+
+  invisible(object)
 })
