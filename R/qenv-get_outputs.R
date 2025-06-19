ec49b8d205ca946a8572130b89f047f0d0be8e49
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2025-06-19 07:51:00 +0200

    fix R CMD check

--- R/qenv-get_outputs.R
+++ R/qenv-get_outputs.R
@@ -1,9 +1,10 @@
 #' Get outputs
 #'
+#' @description
 #' `eval_code` evaluates code silently so plots and prints don't show up in the console or graphic devices.
-#' If one wants to use an output outside of the qenv (e.g. use a graph in `renderPlot`) then use `get_outputs`.
+#' If one wants to use an output outside of the `qenv` (e.g. use a graph in `renderPlot`) then use `get_outputs`.
 #' @param object (`qenv`)
-#' @return list of outputs generated in a qenv
+#' @return list of outputs generated in a `qenv``
 #' @examples
 #' q <- eval_code(
 #'   qenv(),
@@ -14,8 +15,12 @@
 #'   })
 #' )
 #' get_outputs(q)
+#'
+#' @aliases get_outputs,qenv-method
+#'
 #' @export
 setGeneric("get_outputs", function(object) standardGeneric("get_outputs"))
+
 
 
 setMethod("get_outputs", signature = "qenv", function(object) {
--- tests/testthat/test-get_outputs.R
+++ tests/testthat/test-get_outputs.R
@@ -1,8 +1,40 @@
-testthat::test_that("", {
-  q <- qenv()
-  q1 <- eval_code(q, expression(iris, mtcars))
-  testthat::expect_identical(
-    get_outputs(q1),
-    list(iris, mtcars)
-  )
+testthat::describe("get_output", {
+  testthat::it("returns an empty list if nothing is printed", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(a <- 1L, b <- 2L))
+    testthat::expect_identical(get_outputs(q1), list())
+  })
+
+  testthat::it("implicitly printed objects are returned asis in a list", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(a <- 1L, a, b <- 2L, b))
+    testthat::expect_identical(get_outputs(q1), list(1L, 2L))
+  })
+
+  testthat::it("explicitly printed objects are returned as console-output-string in a list", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(a <- 1L, print(a), b <- 2L, print(b)))
+    testthat::expect_identical(get_outputs(q1), list("[1] 1\n", "[1] 2\n"))
+  })
+
+  testthat::it("printed plots are returned as recordedplot in a list", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(a <- 1L, plot(a)))
+    testthat::expect_true(inherits(get_outputs(q1)[[1]], "recordedplot"))
+  })
+
+  testthat::it("warnings are returned asis in a list", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(warning("test")))
+    expected <- simpleWarning("test")
+    expected["call"] <- NULL
+    testthat::expect_identical(get_outputs(q1), list(expected))
+  })
+
+  testthat::it("messages are returned asis in a list", {
+    q <- qenv()
+    q1 <- eval_code(q, expression(message("test")))
+    expected <- simpleMessage("test\n", call = quote(message("test")))
+    testthat::expect_identical(get_outputs(q1), list(expected))
+  })
 })
