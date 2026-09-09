5db9b00cfaee461b256a8eb3d21640ff0600703a
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-09-09 09:08:28 +0200

    Fix assignment parent environment

--- tests/testthat/test-qenv_get_code.R
+++ tests/testthat/test-qenv_get_code.R
@@ -1,4 +1,4 @@
-pasten <<- function(...) paste(..., collapse = "\n")
+pasten <- function(...) paste(..., collapse = "\n")
 
 test_that("get_code returns character of length 0 if no code", {
   expect_identical(get_code(qenv()), "")
